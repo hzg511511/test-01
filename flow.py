230352d2692207ba594998fc7c37477791035731
huzg <54404667+hzg511511@users.noreply.github.com>
2025-09-19 15:22:56 +0800

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,15 +1,5 @@
-import httpx
 from prefect import flow
 
-@flow
-def get_repo_info():
-    #url = "https://api.github.com/repos/PrefectHQ/prefect"
-    url = "https://github.com/PrefectHQ/prefect.git"
-    response = httpx.get(url)
-    #response.raise_for_status()
-    #repo = response.json()
-    print(f"PrefectHQ/prefect repository statistics:")
-
-
-if __name__ == "__main__":
-    get_repo_info()
+@flow(log_prints=True)
+def hello_world():
+    print(f"Hello world from Prefect!")
