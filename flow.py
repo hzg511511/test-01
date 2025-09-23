64297c5829eb9aa014e93e9fafdc8f1629926591
huzg <54404667+hzg511511@users.noreply.github.com>
2025-09-23 11:24:37 +0800

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,4 +1,5 @@
 from prefect import flow, task
+import time
 
 # 1. 定义任务：封装打印逻辑
 @task(log_prints=True)  # 启用任务的日志打印
@@ -8,5 +9,6 @@ def print_hello():
 # 2. 定义流程：调用任务
 @flow(log_prints=True)
 def hello_world():
+    time.sleep(10)
     # 调用任务（Prefect会自动跟踪该任务的状态）
     print_hello()
